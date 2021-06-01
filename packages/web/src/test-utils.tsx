46932fb1ea18dac72ac18620ad64433785ed85f1
Vincent <thisoneis4business@gmail.com>
2021-06-01 20:59:05 +0700

    🏹 Updated actions (Local trials is a bit messy)

--- packages/web/src/test-utils.tsx
+++ packages/web/src/test-utils.tsx
@@ -1,12 +1,20 @@
-import * as React from "react"
-import { render, RenderOptions } from "@testing-library/react"
-import { ChakraProvider, theme } from "@chakra-ui/react"
+import { ChakraProvider, ColorModeScript, theme } from "@chakra-ui/react";
+import { render, RenderOptions } from "@testing-library/react";
+import * as React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 const AllProviders = ({ children }: { children?: React.ReactNode }) => (
-  <ChakraProvider theme={theme}>{children}</ChakraProvider>
-)
+  <>
+    <ColorModeScript />
+    <ChakraProvider theme={theme}>
+      <QueryClientProvider client={new QueryClient()}>
+        {children}
+      </QueryClientProvider>
+    </ChakraProvider>
+  </>
+);
 
 const customRender = (ui: React.ReactElement, options?: RenderOptions) =>
-  render(ui, { wrapper: AllProviders, ...options })
+  render(ui, { wrapper: AllProviders, ...options });
 
-export { customRender as render }
+export { customRender as render };
